c92d87b48ffeaeaab00085834a7d1f26ab44efbe
Diego De Granda <diegodegranda@Diegos-MacBook-Pro.local>
2019-12-30 14:28:18 -0600

    arrays ejemplos

--- 12-arrays.js
+++ 12-arrays.js
@@ -1,1 +1,35 @@
-// Un array es una estructura de datos 
+// Un array es una estructura de datos, es un objeto tipo lista de alto nivel.
+
+// Los arrays son objetos de tipo lista cuyo prototipo tiene métodos para realizar operaciones de recorrido y mutación
+
+var frutas = ["Manzana", "Platano", "Cereza", "Freza"]; 
+ console.log(frutas); 
+ console.log(frutas.length); // length es una propiedad del array
+
+ // Acceder (por índice) a un elemento del Array
+ console.log(frutas[0]);
+
+
+// Metodos para trabajar con arrays 
+
+
+// .push();
+
+ var masFrutas = frutas.push("Uvas"); // Esté metodo añadirá "Uvas" añ final del array
+
+// .pop();
+
+ var utlimo = frutas.pop("Uvas"); // Eliminará "Uvas" del final 
+
+// unshift()
+
+var nuevaLogitud = frutas.unshift("Uvas"); // Añade "Uvas" al inicio 
+
+// shift()
+
+var borrarFruta = frutas.shift("Manzana"); // Elimina "Manzana" del inico
+
+// .indexOf();
+
+var posicion = frutas.indexOf("Platano"); // te dará la posición de ese item en el array
+
